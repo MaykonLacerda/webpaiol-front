d3088c3debfd7bcfff61f50040350b5dd0c76d8d
Maykon Lacerda <maykon@smartinnovation.com.br>
2022-09-07 14:11:25 -0300

    feat: create page to register user

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,10 @@
+import { PublicsLayout } from 'layouts/Public';
+import { RegisterTemplate } from 'templates/Register';
+
+export function Register() {
+  return (
+    <PublicsLayout>
+      <RegisterTemplate />
+    </PublicsLayout>
+  );
+}
--- src/routes/publics.tsx
+++ src/routes/publics.tsx
@@ -1,5 +1,7 @@
+import { Register } from 'pages/Register';
 import { IRoutes } from 'types/routes';
 
 export const PUBLICS: IRoutes = {
   Login: { id: '1', element: <div>Faça o login</div>, path: '/login' },
+  Register: { id: '2', element: <Register />, path: '/register' },
 };
