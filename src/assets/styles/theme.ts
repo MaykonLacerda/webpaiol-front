1891fd5977c8eff77abb47b924db69d4ab678d5b
Maykon Lacerda <maykon@smartinnovation.com.br>
2022-09-07 10:06:59 -0300

    feat: implement default spacing

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -8,4 +8,44 @@ const colors = {
   },
 };
 
-export const theme = extendTheme({ colors });
+const sizes = {
+  sizes: {
+    max: 'max-content',
+    min: 'min-content',
+    full: '100%',
+    '3xs': '14rem',
+    '2xs': '16rem',
+    xs: '20rem',
+    sm: '24rem',
+    md: '28rem',
+    lg: '32rem',
+    xl: '36rem',
+    '2xl': '42rem',
+    '3xl': '48rem',
+    '4xl': '56rem',
+    '5xl': '64rem',
+    '6xl': '72rem',
+    '7xl': '80rem',
+    '8xl': '90rem',
+    container: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+  },
+};
+
+const breakpoints = {
+  sm: '320px',
+  md: '768px',
+  lg: '960px',
+  xl: '1200px',
+  '2xl': '1536px',
+};
+
+export const theme = extendTheme({
+  colors,
+  sizes,
+  breakpoints,
+});
